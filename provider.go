0b7d6adf069e6588516e181d2579f204058a6030
magicshui <shuiyuzhe@gmail.com>
2016-03-05 00:48:29 +0800

    delete unused var

--- provider.go
+++ provider.go
@@ -1,7 +1,6 @@
 package dnspod
 
 import (
-	"github.com/hashicorp/terraform/helper/mutexkv"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/terraform"
 )
@@ -26,8 +25,6 @@ func Provider() terraform.ResourceProvider {
 		ConfigureFunc: providerConfigure,
 	}
 }
-
-var qingcloudMutexKV = mutexkv.NewMutexKV()
 
 func providerConfigure(d *schema.ResourceData) (interface{}, error) {
 	config := Config{
